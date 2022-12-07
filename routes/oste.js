4bfb6311b2c167cf2c12ea348da78fac94978a61
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-12-07 09:35:34 +0100

    add methods

--- routes/oste.js
+++ routes/oste.js
@@ -1,7 +1,21 @@
 import authorization from "../middleware/auth.js";
 
 export default function oste(app) {
-  app.get("/oste", authorization, function (request, response) {
+  // app.get("/oste", authorization, function (request, response) {
+  //   response.json([
+  //     {
+  //       id: 1,
+  //       name: "danbo",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "brie",
+  //     },
+  //   ]);
+  //   response.end();
+  // });
+
+  app.all("/oste", authorization, function (request, response) {
     response.json([
       {
         id: 1,
@@ -10,6 +24,19 @@ export default function oste(app) {
       {
         id: 2,
         name: "brie",
+      },
+    ]);
+    response.end();
+  });
+  app.all("/customers", authorization, function (request, response) {
+    response.json([
+      {
+        id: 1,
+        name: "Dan Boesen",
+      },
+      {
+        id: 2,
+        name: "Brian Brie",
       },
     ]);
     response.end();
